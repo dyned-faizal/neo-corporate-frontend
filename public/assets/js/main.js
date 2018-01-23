9a702635727e2d1837e55b8d805a10c025d23597
dyned <fditya@dyned.com>
2018-01-23 16:42:04 +0700

    update jquery function for scroll and adding gitignore for node_modules

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,9 @@
+    /*
+    Hyperspace by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 (function($) {
-
     skel.breakpoints({
         xlarge: '(max-width: 1680px)',
         large:  '(max-width: 1280px)',
@@ -9,9 +13,49 @@
     });
 
     $(function() {
+
         var $window = $(window),
             $body = $('body'),
             $sidebar = $('#sidebar');
+
+        // Hack: Enable IE flexbox workarounds.
+            if (skel.vars.IEVersion < 12)
+                $body.addClass('is-ie');
+
+        // Disable animations/transitions until the page has loaded.
+            if (skel.canUse('transition'))
+                $body.addClass('is-loading');
+
+            $window.on('load', function() {
+                window.setTimeout(function() {
+                    $body.removeClass('is-loading');
+                }, 100);
+            });
+
+        // Forms.
+
+            // Fix: Placeholder polyfill.
+                $('form').placeholder();
+
+            // Hack: Activate non-input submits.
+                $('form').on('click', '.submit', function(event) {
+
+                    // Stop propagation, default.
+                        event.stopPropagation();
+                        event.preventDefault();
+
+                    // Submit form.
+                        $(this).parents('form').submit();
+
+                });
+
+        // Prioritize "important" elements on medium.
+            skel.on('+medium -medium', function() {
+                $.prioritize(
+                    '.important\\28 medium\\29',
+                    skel.breakpoint('medium').active
+                );
+            });
 
         // Sidebar.
             if ($sidebar.length > 0) {
@@ -113,6 +157,7 @@
 
                     },
                     enter: function() {
+
                         // Activate section.
                             $(this).removeClass('inactive');
 
@@ -157,6 +202,14 @@
 
                         }
                     });
+
+    });
+
+    // click next from welcome page
+    $('#nextToContent').click(function() {
+        $('.landing__page').addClass('slide-up');
+        $('.landing__page').hide(800);
+        $('.wrapper').removeClass('is-paused');
     });
 
     // MODAL
